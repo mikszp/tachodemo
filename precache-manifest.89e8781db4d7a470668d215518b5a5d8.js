2f455d623cf5a49e804d38c666ba962c6894efbb
mszpila <szpila.mikolaj@gmail.com>
2021-05-27 23:48:22 +0200

    Updates

---
+++ precache-manifest.89e8781db4d7a470668d215518b5a5d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcef2f0bc6e81da29ba0ab3a96dc59d9",
+    "url": "/tachodemo/index.html"
+  },
+  {
+    "revision": "aae25d744a1026854ee6",
+    "url": "/tachodemo/static/css/main.16bd39e1.chunk.css"
+  },
+  {
+    "revision": "ee0008eca45184f74afd",
+    "url": "/tachodemo/static/js/2.a1d8a5d5.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/tachodemo/static/js/2.a1d8a5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a01744dfcff4411bc1",
+    "url": "/tachodemo/static/js/3.493a6137.chunk.js"
+  },
+  {
+    "revision": "aae25d744a1026854ee6",
+    "url": "/tachodemo/static/js/main.9183f945.chunk.js"
+  },
+  {
+    "revision": "657f8e3dc40e977facea",
+    "url": "/tachodemo/static/js/runtime-main.b462eb24.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/tachodemo/static/media/logo.06e73328.svg"
+  }
+]);
